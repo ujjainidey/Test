28f1d3accb7edf5372cc2ba3ac04d222cee0c092
ujjainidey <41863684+ujjainidey@users.noreply.github.com>
2020-03-13 11:19:16 +0530

    Update App.java

--- src/main/java/Demo/App.java
+++ src/main/java/Demo/App.java
@@ -1,13 +1,21 @@
-package Demo;
+package com.mkyong.examples;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class MessageBuilder {
+
+    public String getMessage(String name) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (name == null || name.trim().length() == 0) {
+
+            result.append("Please provide a name!");
+
+        } else {
+
+            result.append("Hello " + name);
+
+        }
+        return result.toString();
     }
+
 }
